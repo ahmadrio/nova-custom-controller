5b1e91a73481046e6d983587c437b778c28b8be2
Ahmad Rio <ahmadrio28asia@gmail.com>
2019-10-13 22:26:48 +0700

    fix namespace provider

--- src/NovaCustomControllerProvider.php
+++ src/NovaCustomControllerProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PtDotPlayground\NovaCustomControllers;
+namespace PtDotPlayground\NovaCustomController;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
